bca3b1523e0097c88f3d6ae895a61bba71fd15a6
seveibar <seveibar@gmail.com>
2023-11-23 12:55:20 -0800

    responsive header

--- ui/components/Header.tsx
+++ ui/components/Header.tsx
@@ -6,38 +6,43 @@ export const Header = ({ noShadow = false }: { noShadow?: boolean }) => (
       noShadow ? "" : "shadow-md border-b"
     }z-10 relative`}
   >
-    <div className="container mx-auto flex items-center">
-      <a
-        className="bg-blue-500 text-white inline-flex p-1 px-2 font-semibold rounded-lg"
-        href="https://tscircuit.com"
-      >
-        tscircuit
-      </a>
-      <div className="ml-3 text-xs text-gray-600">
-        Registry
-        <br />
-        v0.0.1
+    <div className="container mx-auto flex items-center flex-col sm:flex-row">
+      <div className="flex items-center mb-4 sm:mb-0">
+        <a
+          className="bg-blue-500 text-white inline-flex p-1 px-2 font-semibold rounded-lg"
+          href="https://tscircuit.com"
+        >
+          tscircuit
+        </a>
+        <div className="ml-3 text-xs text-gray-600">
+          Registry
+          <br />
+          v0.0.1
+        </div>
+        <a href="https://github.com/tscircuit/tscircuit" className="ml-3">
+          <FaGithub style={{ fontSize: 18 }} className="text-gray-600" />
+        </a>
       </div>
-      <a href="https://github.com/tscircuit/tscircuit" className="ml-3">
-        <FaGithub style={{ fontSize: 18 }} className="text-gray-600" />
-      </a>
-      <div className="ml-4">
-        {["Trending Packages", "How to Publish", "Getting Started"].map(
-          (item) => (
-            <a
-              key={item}
-              className="inline-flex ml-6 text-xs text-gray-700 font-medium cursor-pointer"
-            >
-              {item}
-            </a>
-          )
-        )}
+      <div className="flex flex-col md:flex-row flex-grow items-center">
+        <div className="md:ml-4">
+          {["Trending Packages", "How to Publish", "Getting Started"].map(
+            (item) => (
+              <a
+                key={item}
+                className="inline-flex ml-6 text-xs text-gray-700 font-medium cursor-pointer whitespace-nowrap"
+              >
+                {item}
+              </a>
+            )
+          )}
+        </div>
+        <div className="flex flex-grow items-center mt-4 md:mt-0 justify-end">
+          <a className="inline-flex text-gray-700 text-xs w-10 font-medium mr-4 cursor-pointer whitespace-nowrap">
+            Sign In
+          </a>
+          <SearchBar />
+        </div>
       </div>
-      <div className="md:flex-grow" />
-      <a className="inline-flex text-gray-700 text-xs font-medium mr-6 cursor-pointer">
-        Sign In
-      </a>
-      <SearchBar />
     </div>
   </div>
 )
