1a3e223c847fadadb0091eeafbfe17c7280b9d6f
seveibar <seveibar@gmail.com>
2023-12-10 09:34:51 -0800

    sometimes use local registry

--- ui/lib/get-registry-axios.ts
+++ ui/lib/get-registry-axios.ts
@@ -5,8 +5,18 @@ export const getRegistryAxios = ({
   registryToken?: string
 } = {}) => {
   // TODO detect if local and use local registry api
+
+  let apiBaseUrl = "https://registry-api.tscircuit.com"
+
+  if (
+    typeof window !== "undefined" &&
+    window.location.href.includes("localhost")
+  ) {
+    apiBaseUrl = "http://localhost:3100"
+  }
+
   const registryAxios = defaultAxios.create({
-    baseURL: "https://registry-api.tscircuit.com",
+    baseURL: apiBaseUrl,
     headers: {
       Authorization: `Bearer ${registryToken}`,
     },
